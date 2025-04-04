8325a7cb7db09a32bd588285197b8148fce51475
Krishna Purwar <krishnapurwar23022003@gmail.com>
2025-04-04 11:12:47 +0530

    Added css to sign-in and sign-up

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,8 +3,10 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <SignIn redirectUrl="/dashboard"/>
+    <div className="h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <SignIn redirectUrl="/dashboard"/>
+      </div>
     </div>
   )
 }
