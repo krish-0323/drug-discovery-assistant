57cb09594f8b638731c8e73b8a14874e4dc56fa8
Krishna Purwar <krishnapurwar23022003@gmail.com>
2025-04-06 21:31:40 +0530

    openai.ts updated

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,56 +1,56 @@
-import { Configuration, OpenAIApi } from "openai-edge";
+// import { Configuration, OpenAIApi } from "openai-edge";
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const config = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
-const openai = new OpenAIApi(config);
+// const openai = new OpenAIApi(config);
 
-export async function generateImagePrompt(name: string) {
-  try {
-    const response = await openai.createChatCompletion({
-      model: "gpt-4o-mini",
-      messages: [
-        {
-          role: "system",
-          content:
-            "You are an creative and helpful AI assistance capable of generating interesting thumbnail descriptions for my notes. Your output will be fed into the DALLE API to generate a thumbnail. The description should be minimalistic and flat styled",
-        },
-        {
-          role: "user",
-          content: `Please generate a thumbnail description for my notebook titles ${name}`,
-        },
-      ],
-    });
-    const data = await response.json();
+// export async function generateImagePrompt(name: string) {
+//   try {
+//     const response = await openai.createChatCompletion({
+//       model: "gpt-4o-mini",
+//       messages: [
+//         {
+//           role: "system",
+//           content:
+//             "You are an creative and helpful AI assistance capable of generating interesting thumbnail descriptions for my notes. Your output will be fed into the DALLE API to generate a thumbnail. The description should be minimalistic and flat styled",
+//         },
+//         {
+//           role: "user",
+//           content: `Please generate a thumbnail description for my notebook titles ${name}`,
+//         },
+//       ],
+//     });
+//     const data = await response.json();
 
-    console.log(data);
+//     console.log(data);
     
-    // const image_description = data.choices[0].message.content;
-    const image_description = "A notebook with a minimalistic flat design";
+//     // const image_description = data.choices[0].message.content;
+//     const image_description = "A notebook with a minimalistic flat design";
 
-    return image_description as string;
+//     return image_description as string;
 
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-}
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// }
 
-export async function generateImage(image_description: string) {
-  try {
+// export async function generateImage(image_description: string) {
+//   try {
     
-    // const response = await openai.createImage({
-    //   prompt: image_description,
-    //   n: 1,
-    //   size: "256x256",
-    // });
-    // const data = await response.json();
-    // const image_url = data.data[0].url;
+//     // const response = await openai.createImage({
+//     //   prompt: image_description,
+//     //   n: 1,
+//     //   size: "256x256",
+//     // });
+//     // const data = await response.json();
+//     // const image_url = data.data[0].url;
     
-    const image_url = "thumbnail.webp"
-    return image_url as string;
-  } catch (error) {
-    console.error(error);
-  }
-}
+//     const image_url = "thumbnail.webp"
+//     return image_url as string;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
